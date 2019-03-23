c2182c26a0f6ec89c0df1aaf71fb3aa5c8547363
Sergey Balakin <sergey.balakin1993@gmail.com>
2019-03-23 14:40:37 +0300

    Comments for code

--- app/src/main/java/com/example/javalesson/javalesson1/MainActivity.java
+++ app/src/main/java/com/example/javalesson/javalesson1/MainActivity.java
@@ -8,29 +8,29 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
-    TextView textView;
+    Button button; //Говорим, что у нас есть такая переменная "Кнопка" (объявляем переменную). Эта переменная пока пустая
+    TextView textView; //Говорим, что у нас есть такая переменная "Text" (объявляем переменную). Эта переменная пока пустая
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        textView = findViewById(R.id.textView);
+        setContentView(R.layout.activity_main);//Подключаем файл с интерфейсом (лейоут)
+        button = findViewById(R.id.button); //Находим там кнопку
+        textView = findViewById(R.id.textView); //Находим текст
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {//говорим, что произойдет если нажать на кнопку
             @Override
             public void onClick(View view) {
-                myAmazingMethod();
+                myAmazingMethod();//при нажатии на кнопку вызовется вот этот код (метод)
             }
         });
     }
 
-    private void myAmazingMethod(){
-        String result = "Что-то новенькое";
+    void myAmazingMethod(){//это наш изумительный метод
+        String result = "Что-то новенькое"; //создаем текстовую строчку и присваиваем ей значение "Что-то новенькое"
 
 
-        textView.setText(result);
+        textView.setText(result);//Отображаем на нашей TextView текстовую строчку result
     }
 
 }
